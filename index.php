dbcb7a8ff6be39e08cb996cd370d596eabb67ba7
bsantalucia <beatriz.santalucia@usp.br>
2015-01-09 23:17:12 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cubos</title>
+		<meta charset="utf-8">
+		<link href="css/default.css" rel="stylesheet" type="text/css" media="screen" />
+		<link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Carme' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div id="login">
+			<ul>
+				<li><a href="php/login.php">LOGIN</a></li>
+				<li><a href="php/login_cadastro.php">CADASTRE-SE</a></li>	
+			</ul>
+		</div>
+		<div id="logo">
+			<img src="images/logo.png" alt="Logo" width="1024px" height="230px" >	
+		</div>
+		<div id="menu">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="jogos/jogos.html">Jogos</a></li>
+				<li><a href="sobre.html">Sobre</a></li>
+				<li><a href="contato.html">Contato</a></li>
+			</ul>
+		</div>
+		<div id="conteudo">
+			<div id="conteudo_home">
+				<h2>Bem Vindo(a) !</h2>
+				<p>
+					Você está prestes a entrar no maravilhoso mundo de Cubo Games! Onde os melhores jogos estão. Divirta-se com o Jogo da Memória do Sr. Panda! Encontre os mais diversos animais! Encontre todos os pares antes que o tempo acabe!
+
+
+					<br><br>
+					
+				</p>
+			</div>
+		</div>
+	</body>
+</html>
